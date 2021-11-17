2f1b93496f46abf81d9cac7ac2fe8ccc794cbe07
evgeniya35 <evgeniya35@yandex.ru>
2021-11-17 11:42:13 +0300

    Скрипты работы с базой

---
+++ db_helpers.py
@@ -0,0 +1,73 @@
+import datetime
+
+from sqlalchemy.engine import base
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+Base = automap_base()
+
+engine = create_engine(r"sqlite:///SelfStorage.db")
+
+Base.prepare(engine, reflect=True)
+
+Warehouses = Base.classes.warehouses
+Boxes = Base.classes.boxes
+Clients = Base.classes.clients
+Storages = Base.classes.storages
+Prices = Base.classes.prices
+Orders = Base.classes.orders
+
+session = Session(engine)
+
+def get_warehouses():
+    return session.query(Warehouses).all()
+
+
+def get_clients():
+    return session.query(Clients).all()
+
+
+def get_boxes():
+    return session.query(Boxes).all()
+
+
+def get_storages():
+    return session.query(Storages).all()
+
+
+def get_prices(**kwargs):
+    return session.query(Prices).filter_by().all()
+
+
+def get_orders():
+    return session.query(Orders).all()
+
+
+def add_client(context_data):
+    new_client = Clients(
+                    title = context_data['title'],
+                    fio = context_data['fio'],
+                    phone = context_data['phone'],
+                    pass_id = context_data['pass_id'],
+                    birth_date = context_data['birth_date'],
+                    description = context_data['description'],
+                )
+    session.add(new_client)
+    session.commit()
+    return new_client.id
+
+
+def add_order(context_data):
+    new_order = Orders(
+                    title = context_data['title'],
+                    client_id = context_data['client_id'],
+                    storage_id = context_data['storage_id'],
+                    box_id = context_data['box_id'],
+                    rent_from = context_data['rent_from'],
+                    rent_to = context_data['rent_to'],
+                    description = context_data['description'],
+                )
+    session.add(new_order)
+    session.commit()
+    return new_order.id
