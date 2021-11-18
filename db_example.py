3a0a28694a27c2eecef7da17ac024877f949b977
evgeniya35 <evgeniya35@yandex.ru>
2021-11-18 11:58:09 +0300

    Генерация qr

--- db_example.py
+++ db_example.py
@@ -1,14 +1,16 @@
 import datetime
+
 from db_helpers import Warehouses, Boxes, Clients, Storages, Prices, Orders
 from db_helpers import get_records, get_records_sql, add_client, add_order
 
 
-# Таблица, фильтр (опционально). Возвращает список строк, строка словарь
+
+""" Таблица, фильтр (опционально). Возвращает список строк, строка словарь
 filter = {'title': 'Склад Юг'}
 for row in get_records(Warehouses, filter):
     print(row.id, row.title, row.__dict__)
 
-# Весь клиенты
+# Все клиенты
 for row in get_records(Clients):
     print(row.id, row.title, row.__dict__)
 
@@ -17,7 +19,7 @@ title = 'сноуборд за место'
 period = 'неделя'
 print(get_records_sql(f"SELECT * FROM v_prices WHERE title = '{title}' AND period = '{period}'"))
 
-""" С прайсом работать через произвольный запрос к view "v_prices":
+''' С прайсом работать через произвольный запрос к view "v_prices":
 id	title	period	price	storage_id	storage_title	storage_path
 1	4 колеса	месяц	400	2	колёса	сезонные/колёса
 2	лыжи за место	месяц	300	3	лыжи	сезонные/лыжи
@@ -27,7 +29,7 @@ id	title	period	price	storage_id	storage_title	storage_path
 6	сноуборд за место	месяц	300	4	сноуборд	сезонные/сноуборд
 7	сноуборд за место	неделя	100	4	сноуборд	сезонные/сноуборд
 8	за 1 кв. метр	месяц	599	6	другое	другое
-"""
+'''
 
 # Произвольный запрос на выборку
 print(get_records_sql('SELECT * FROM v_prices'))
@@ -36,8 +38,17 @@ title = 'сноуборд за место'
 period = 'неделя'
 print(get_records_sql(f"SELECT * FROM v_prices WHERE title = '{title}' AND period = '{period}'"))
 
+# Генерация qr кода
+context_data = {'order_id': 1,
+ 'order_date': '01.11.2021',
+ 'order_title': 'Аренда для велосипеда',
+ 'rent_from': '10.11.2021',
+ 'rent_to': '10.12.2021',
+ 'client_fio': 'Иванов Пётр Сидорович'
+ }
+img = generate_qr(context_data)
+img.save(f'qr_{context_data["order_id"]}.png')
 
-"""
 # Добавление нового клиента, возвращает id
 print(
     add_client({'title': 'Клиент 5',
@@ -52,12 +63,25 @@ print(
 # Добавление нового заказа, возвращает id
 print(
     add_order({'title': 'Заказ важный',
+                'order_date': datetime.date(2021, 10, 1),
                 'client_id': 2,
                 'storage_id': 3,
-                'box_id': 12,
+                'wrh_id': 1,
                 'rent_from': datetime.date(2021, 10, 17),
                 'rent_to': datetime.date(2021, 12, 17),
                 'description': 'Описание'
                 })
 )
 """
+print(
+    add_order({'title': 'Заказ важный',
+                'order_date': datetime.date(2021, 10, 1),
+                'client_id': 2,
+                'storage_id': 3,
+                'storage_cnt': 4,
+                'wrh_id': 1,
+                'rent_from': datetime.date(2021, 10, 17),
+                'rent_to': datetime.date(2021, 12, 17),
+                'description': 'Описание'
+                })
+)
--- db_helpers.py
+++ db_helpers.py
@@ -1,5 +1,7 @@
 import datetime
 
+import qrcode
+
 from sqlalchemy.engine import base
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -20,6 +22,7 @@ Orders = Base.classes.orders
 
 session = Session(engine)
 
+
 def get_records(table, filter={}):
     if filter:
         return session.query(table).filter_by(**filter).all()
@@ -33,13 +36,13 @@ def get_records_sql(sql):
 
 def add_client(context_data):
     new_client = Clients(
-                    title = context_data['title'],
-                    fio = context_data['fio'],
-                    phone = context_data['phone'],
-                    pass_id = context_data['pass_id'],
-                    birth_date = context_data['birth_date'],
-                    description = context_data['description'],
-                )
+        title=context_data['title'],
+        fio=context_data['fio'],
+        phone=context_data['phone'],
+        pass_id=context_data['pass_id'],
+        birth_date=context_data['birth_date'],
+        description=context_data['description'],
+    )
     session.add(new_client)
     session.commit()
     return new_client.id
@@ -47,14 +50,34 @@ def add_client(context_data):
 
 def add_order(context_data):
     new_order = Orders(
-                    title = context_data['title'],
-                    client_id = context_data['client_id'],
-                    storage_id = context_data['storage_id'],
-                    box_id = context_data['box_id'],
-                    rent_from = context_data['rent_from'],
-                    rent_to = context_data['rent_to'],
-                    description = context_data['description'],
-                )
+        title=context_data['title'],
+        order_date=context_data['order_date'],
+        client_id=context_data['client_id'],
+        storage_id=context_data['storage_id'],
+        storage_cnt=context_data['storage_cnt'],
+        wrh_id=context_data['wrh_id'],
+        rent_from=context_data['rent_from'],
+        rent_to=context_data['rent_to'],
+        description=context_data['description'],
+    )
     session.add(new_order)
     session.commit()
     return new_order.id
+
+
+def generate_qr(context_data):
+    text = (
+        f'{context_data["client_fio"]}.\n'
+        f'Заказ № {context_data["order_id"]}, '
+        f'от {context_data["order_date"]} '
+        f'"{context_data["order_title"]}"\n'
+        f'Аренда с {context_data["rent_to"]} по '
+        f'{context_data["order_date"]}\n'
+        f'Сгенерировано {datetime.now().strftime("%d.%m.%Y %H:%M:%S")}'
+    )
+    qr = qrcode.QRCode(version=1, box_size=6, border=3)
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    # img.save(f'qr{context_data["order_id"]}.png')
+    return img
